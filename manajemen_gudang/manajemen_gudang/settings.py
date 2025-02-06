87ddfea8032ed960bf558cfc6aad4f3be2d14686
Rizqulloh Rayhan Ferdiansyah <rizqullohrayhan@student.uns.ac.id>
2025-02-06 15:38:38 +0700

    change src at vercel.json

--- manajemen_gudang/manajemen_gudang/settings.py
+++ manajemen_gudang/manajemen_gudang/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
